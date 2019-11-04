8e7b78a51d9dc5f68abe24eca1ca425786cdf3b3
Felix Kahmann <felix@kahmann.com>
2019-11-04 21:51:23 +0100

    improve public export structure

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,8 @@ function create<T>(source: Iterable<T>) {
   return new IterableTransform(source)
 }
 
+export * from "./array-transforms"
+export * from "./transforms"
+export * from "./types"
+
 export default create
